b4f0d2589b5f7bd2f53d5c3a65115fa447660512
VTarasenko <vtarasenko@playnext.ru>
2020-05-03 00:04:22 +0300

    Feat: PaletteBlock + Sliders (faq, info)

---
+++ src/components/elements/PaletteBlock.vue
@@ -0,0 +1,78 @@
+<template>
+	<b-container>
+		<b-row class="palette-block">
+			<b-col cols="12" sm="3" offset-sm="3">
+				<h5 data-aos="fade-up">Логотип</h5>
+				<div data-aos="fade-up" data-aos-delay="200" class="palette-block__logo" :style="`background-image: url(${logo})`" />
+			</b-col>
+			<b-col data-aos="fade-up" cols="12" sm="4" offset-sm="1" class="d-flex flex-column">
+				<h5 data-aos="fade-up" data-aos-delay="200">Шрифт</h5>
+				<h3 data-aos="fade-up" data-aos-delay="400">SF Pro Text</h3>
+				<h5 data-aos="fade-up" data-aos-delay="600">Палитра</h5>
+				<div class="palette-colors d-flex">
+					<div
+						class="palette-colors__item"
+						v-for="(item, index) in colors"
+						:key="index"
+						:style="item"
+						 data-aos="fade-left"
+						:data-aos-delay="1000+index*100"
+					/>
+				</div>
+			</b-col>
+		</b-row>
+	</b-container>
+</template>
+
+<script>
+export default {
+	name: "PaletteBlock",
+	props: {
+		colors: {
+			type: Array,
+			default: () => []
+		}
+	},
+	data: () => ({
+		logo: require("../../assets/img/logo.png")
+	})
+};
+</script>
+
+<style lang="scss">
+.palette {
+	&-block {
+		padding: $gutter-lg 0;
+
+		h5 {
+			font-weight: normal;
+			margin-bottom: $gutter;
+
+			&:last-of-type {
+				margin-top: auto;
+			}
+		}
+
+		&__logo {
+			width: 200px;
+			height: 200px;
+			background-size: contain;
+			background-repeat: no-repeat;
+		}
+	}
+
+	&-colors {
+		&__item {
+			width: 60px;
+			height: 60px;
+			box-shadow: 0 12px 34px 0 rgba(0, 0, 0, 0.15);
+			border-radius: 50%;
+			margin-right: $gutter;
+
+			&:last-child {
+				margin: 0;
+			}
+		}
+	}
+}
+</style>
---
+++ src/components/elements/SliderInfo.vue
@@ -0,0 +1,79 @@
+<template>
+	<b-container class="d-flex flex-wrap">
+        <h2 data-aos="fade-up" class="section-title flex-grow-1" v-if="title">{{ title }}</h2>
+        <div  data-aos="fade-up" class="slider-info-nav d-flex align-self-center">
+            <div class="slider-info-nav__prev" slot="button-prev"></div>
+            <div class="slider-info-nav__next" slot="button-next"></div>
+        </div>
+		<swiper  data-aos="fade-up" class="slider-info" ref="mySwiper" :options="swiperOptions">
+			<swiper-slide class="slider-info__item" v-for="(item, index) in 9" :key="index">
+                <h5>Сортировка фотографий из фотоальбома</h5>
+                <p>Мы использовали такие-то технологии, чтобы сделать эту функцию</p>
+            </swiper-slide>
+		</swiper>
+	</b-container>
+</template>
+
+<script>
+export default {
+    name: "SliderInfo",
+    props: {
+        title: {
+            type: String,
+            default: null,
+        }
+    },
+	data: () => ({
+		swiperOptions: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+            speed: 900,
+            spaceBetween: 15,
+			navigation: {
+				nextEl: ".slider-info-nav__next",
+				prevEl: ".slider-info-nav__prev"
+			}
+		}
+	})
+};
+</script>
+
+<style lang="scss">
+.slider-info {
+    cursor: grab;
+    margin: $gutter-sm 0 $gutter-lg;
+
+    &__item {
+        padding: $gutter-sm 35px;
+        height: 300px;
+        background: $white;
+        border-radius: $border-radius;
+
+        h5 {
+            margin-bottom: $gutter;
+        }
+    }
+
+    &-nav {
+        position: relative;
+
+        &__prev, &__next {
+            width: 63px;
+            height: 63px;
+            border-radius: 50%;
+            background: grey;
+            z-index: 1;
+            outline: none;
+            cursor: pointer;
+        }
+
+        &__prev {
+            margin-right: $gutter;
+        }
+    }
+
+    &:active {
+        cursor: grabbing;
+    }
+}
+</style>
